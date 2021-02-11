cb8c05c0dde351529d5561fb0dd66fe67934efab
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-02-11 03:32:01 +0200

    Test a State concept

--- components/TestScreen.tsx
+++ components/TestScreen.tsx
@@ -22,12 +22,38 @@ interface TestScreenProps {
 }
 
 export default class TestScreen extends React.Component<TestScreenProps> {
+  state = { count: 0, date: new Date() };
+  timer: NodeJS.Timeout | null = null;
+
+  componentDidMount() {
+    this.timer = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    if (this.timer) clearInterval(this.timer);
+  }
+
+  tick() {
+    this.setState({ ...this.state, date: new Date() });
+  }
+
+  handleClick() {
+    // this.props.navigation?.goBack();
+    this.setState({ ...this.state, count: this.state.count + 1 });
+
+    // Update state in asynchronous case
+    // this.setState((state, props) => ({ ...state, count: state.count + 1 }));
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Count: {this.state.count}</Text>
+        <Text style={{ ...styles.text, color: "#F00" }}>What time is it?</Text>
+        <Text style={styles.text}>It's {this.state.date.toLocaleTimeString()}</Text>
         <Image source={require("../assets/icons/Icon-144.png")} />
-        <Button title="Home" onPress={() => this.props.navigation?.goBack()} />
+        <Button title="Count" onPress={() => this.handleClick()} />
       </View>
     );
   }
@@ -39,5 +65,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 18,
   },
 });
