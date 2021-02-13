63ffc8eb38f38b1b0d2b715b70444964515a1dba
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-02-13 04:05:19 +0200

    Use index file for load all required images

---
+++ assets/index.ts
@@ -0,0 +1,24 @@
+export const Images = {
+  icons: {
+    "Icon-36": require("./icons/Icon-36.png"),
+    "Icon-48": require("./icons/Icon-48.png"),
+    "Icon-72": require("./icons/Icon-72.png"),
+    "Icon-96": require("./icons/Icon-96.png"),
+    "Icon-144": require("./icons/Icon-144.png"),
+    "Icon-192": require("./icons/Icon-192.png"),
+    "Icon-512": require("./icons/Icon-512.png"),
+  },
+  BookCover: {
+    Image_01: require("./BookCover/Image_01.png"),
+    Image_02: require("./BookCover/Image_02.png"),
+    Image_03: require("./BookCover/Image_03.png"),
+    Image_05: require("./BookCover/Image_05.png"),
+    Image_06: require("./BookCover/Image_06.png"),
+    Image_07: require("./BookCover/Image_07.png"),
+    Image_08: require("./BookCover/Image_08.png"),
+    Image_10: require("./BookCover/Image_10.png"),
+  },
+};
+
+export type BookCover = "Image_01" | "Image_02" | "Image_03" | "Image_05" | "Image_06" | "Image_07" | "Image_08" | "Image_10";
+export type Icons = "Icon-36" | "Icon-48" | "Icon-72" | "Icon-96" | "Icon-144" | "Icon-192" | "Icon-512";
