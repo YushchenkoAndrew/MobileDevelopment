203a3664906d918bac320e25c64f9872d2bb81f3
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-02-15 02:20:11 +0200

    Add required files to the project

--- assets/index.ts
+++ assets/index.ts
@@ -1,4 +1,4 @@
-export const Images = {
+export default {
   icons: {
     "Icon-36": require("./icons/Icon-36.png"),
     "Icon-48": require("./icons/Icon-48.png"),
@@ -18,7 +18,30 @@ export const Images = {
     Image_08: require("./BookCover/Image_08.png"),
     Image_10: require("./BookCover/Image_10.png"),
   },
+  BookInfo: {
+    "9780321856715": require("./BookInfo/9780321856715.json"),
+    "9780321862969": require("./BookInfo/9780321862969.json"),
+    "9781118841471": require("./BookInfo/9781118841471.json"),
+    "9781430236054": require("./BookInfo/9781430236054.json"),
+    "9781430237105": require("./BookInfo/9781430237105.json"),
+    "9781430238072": require("./BookInfo/9781430238072.json"),
+    "9781430245124": require("./BookInfo/9781430245124.json"),
+    "9781430260226": require("./BookInfo/9781430260226.json"),
+    "9781449308360": require("./BookInfo/9781449308360.json"),
+    "9781449342753": require("./BookInfo/9781449342753.json"),
+  },
 };
 
 export type BookCover = "Image_01" | "Image_02" | "Image_03" | "Image_05" | "Image_06" | "Image_07" | "Image_08" | "Image_10";
 export type Icons = "Icon-36" | "Icon-48" | "Icon-72" | "Icon-96" | "Icon-144" | "Icon-192" | "Icon-512";
+export type BookInfo =
+  | "9780321856715"
+  | "9780321862969"
+  | "9781118841471"
+  | "9781430236054"
+  | "9781430237105"
+  | "9781430238072"
+  | "9781430245124"
+  | "9781430260226"
+  | "9781449308360"
+  | "9781449342753";
