9fb5ca58b425deac5d0cc73110cdeff67a98be2d
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-02-27 03:23:54 +0200

    Create proper grid defined by task, implement increaseing list

--- components/Screen/ImageListScreen.tsx
+++ components/Screen/ImageListScreen.tsx
@@ -1,20 +1,59 @@
+import * as ScreenOrientation from "expo-screen-orientation";
 import * as React from "react";
-import { StyleSheet, View } from "react-native";
+import { Button, Dimensions, StyleSheet, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
+
+const { width, height } = Dimensions.get("screen");
 
 export interface ImageListScreenProps {}
 
 export default class ImageListScreen extends React.PureComponent<ImageListScreenProps> {
-  state = { images: new Array(20).fill(0) };
+  state = { images: new Array(7).fill(0), grid: [], orientation: ScreenOrientation.Orientation.UNKNOWN };
+  index = 0;
+
+  componentDidMount() {
+    ScreenOrientation.getOrientationAsync().then((orientation) => this.setState({ ...this.state, orientation }));
+  }
+
+  componentWillUnmount() {
+    ScreenOrientation.removeOrientationChangeListeners();
+  }
+
+  next() {
+    const { grid } = this.state;
+    const size = (this.state.orientation == ScreenOrientation.Orientation.PORTRAIT_UP ? width : height) / 3;
+
+    switch (this.index++ % 9) {
+      case 4:
+        this.setState({ ...this.state, grid: [...grid, <View key={this.index} style={{ ...styles.box, width: size * 2 - 5, height: size * 2 - 5 }} />] });
+        break;
+
+      case 5:
+        this.setState({
+          ...this.state,
+          grid: [
+            ...grid.slice(0, -2),
+            <View key={this.index} style={{ flexDirection: "row" }}>
+              <View key={this.index} style={{ flexDirection: "column" }}>
+                <View style={{ ...styles.box, width: size - 5, height: size - 5 }} />
+                <View style={{ ...styles.box, width: size - 5, height: size - 5 }} />
+              </View>
+              <View style={{ ...styles.box, width: size * 2 - 5, height: size * 2 - 5 }} />
+            </View>,
+          ],
+        });
+        break;
+
+      default:
+        this.setState({ ...this.state, grid: [...grid, <View key={this.index} style={{ ...styles.box, width: size - 5, height: size - 5 }} />] });
+    }
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView contentContainerStyle={{ flexWrap: "wrap", flexDirection: "row" }}>
-          {this.state.images.map((image, index) => (
-            <View key={index} style={styles.box} />
-          ))}
-        </ScrollView>
+        <ScrollView contentContainerStyle={{ flexWrap: "wrap", flexDirection: "row" }}>{this.state.grid}</ScrollView>
+        <Button title="Press" onPress={() => this.next()} />
       </View>
     );
   }
@@ -24,18 +63,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+    // alignItems: "center",
     justifyContent: "center",
   },
 
   box: {
-    // flex: 1,
+    // flex: 2,
     backgroundColor: "powderblue",
 
     alignSelf: "flex-start",
+    justifyContent: "flex-start",
     margin: 2,
-
-    height: 50,
-    width: 50,
   },
 });
