095ba35f4f8ef93d5a5e4bd2b46ceb10051c368a
YushchenkoAndrew <Andrew10122000@gmail.com>
2021-02-27 21:36:46 +0200

    Update grid aligment with an orientation update

--- components/Screen/ImageListScreen.tsx
+++ components/Screen/ImageListScreen.tsx
@@ -9,23 +9,60 @@ export interface ImageListScreenProps {}
 
 export default class ImageListScreen extends React.PureComponent<ImageListScreenProps> {
   state = { images: new Array(7).fill(0), grid: [], orientation: ScreenOrientation.Orientation.UNKNOWN };
+  size = (width < height ? width : height) / 3;
   index = 0;
 
   componentDidMount() {
-    ScreenOrientation.getOrientationAsync().then((orientation) => this.setState({ ...this.state, orientation }));
+    ScreenOrientation.addOrientationChangeListener((rotationEvent) => this.updateOrientation(rotationEvent.orientationInfo.orientation));
   }
 
   componentWillUnmount() {
     ScreenOrientation.removeOrientationChangeListeners();
   }
 
+  updateOrientation(orientation: ScreenOrientation.Orientation) {
+    this.size = (orientation == ScreenOrientation.Orientation.PORTRAIT_UP ? width : height) / 3;
+
+    this.setState({
+      ...this.state,
+      grid: this.state.grid.map(({ id, item }) => {
+        switch (id % 9) {
+          case 4:
+            return { id, item: React.cloneElement(item, { style: { ...styles.box, width: this.size * 2 - 5, height: this.size * 2 - 5 } }) };
+
+          case 5:
+            return {
+              id,
+              item: (
+                <View key={id} style={{ flexDirection: "row" }}>
+                  <View style={{ flexDirection: "column" }}>
+                    <View style={{ ...styles.box, width: this.size - 5, height: this.size - 5 }} />
+                    <View style={{ ...styles.box, width: this.size - 5, height: this.size - 5 }} />
+                  </View>
+                  <View style={{ ...styles.box, width: this.size * 2 - 5, height: this.size * 2 - 5 }} />
+                </View>
+              ),
+            };
+
+          default:
+            return { id, item: React.cloneElement(item, { style: { ...styles.box, width: this.size - 5, height: this.size - 5 } }) };
+        }
+      }),
+    });
+  }
+
   next() {
     const { grid } = this.state;
-    const size = (this.state.orientation == ScreenOrientation.Orientation.PORTRAIT_UP ? width : height) / 3;
 
     switch (this.index++ % 9) {
       case 4:
-        this.setState({ ...this.state, grid: [...grid, <View key={this.index} style={{ ...styles.box, width: size * 2 - 5, height: size * 2 - 5 }} />] });
+        this.setState({
+          ...this.state,
+          grid: [
+            ...grid,
+            { id: this.index - 1, item: <View key={this.index} style={{ ...styles.box, width: this.size * 2 - 5, height: this.size * 2 - 5 }} /> },
+          ],
+        });
         break;
 
       case 5:
@@ -33,26 +70,34 @@ export default class ImageListScreen extends React.PureComponent<ImageListScreen
           ...this.state,
           grid: [
             ...grid.slice(0, -2),
-            <View key={this.index} style={{ flexDirection: "row" }}>
-              <View key={this.index} style={{ flexDirection: "column" }}>
-                <View style={{ ...styles.box, width: size - 5, height: size - 5 }} />
-                <View style={{ ...styles.box, width: size - 5, height: size - 5 }} />
-              </View>
-              <View style={{ ...styles.box, width: size * 2 - 5, height: size * 2 - 5 }} />
-            </View>,
+            {
+              id: this.index - 1,
+              item: (
+                <View key={this.index} style={{ flexDirection: "row" }}>
+                  <View style={{ flexDirection: "column" }}>
+                    <View style={{ ...styles.box, width: this.size - 5, height: this.size - 5 }} />
+                    <View style={{ ...styles.box, width: this.size - 5, height: this.size - 5 }} />
+                  </View>
+                  <View style={{ ...styles.box, width: this.size * 2 - 5, height: this.size * 2 - 5 }} />
+                </View>
+              ),
+            },
           ],
         });
         break;
 
       default:
-        this.setState({ ...this.state, grid: [...grid, <View key={this.index} style={{ ...styles.box, width: size - 5, height: size - 5 }} />] });
+        this.setState({
+          ...this.state,
+          grid: [...grid, { id: this.index - 1, item: <View key={this.index} style={{ ...styles.box, width: this.size - 5, height: this.size - 5 }} /> }],
+        });
     }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView contentContainerStyle={{ flexWrap: "wrap", flexDirection: "row" }}>{this.state.grid}</ScrollView>
+        <ScrollView contentContainerStyle={{ flexWrap: "wrap", flexDirection: "row" }}>{this.state.grid.map(({ item }) => item)}</ScrollView>
         <Button title="Press" onPress={() => this.next()} />
       </View>
     );
@@ -65,6 +110,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     // alignItems: "center",
     justifyContent: "center",
+
+    marginTop: 25,
   },
 
   box: {
